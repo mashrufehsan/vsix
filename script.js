79baaf573cb3a42f86cd8b23b320c0d1d2f8d81b
Mashruf Ehsan <mashruf.ehsan@gmail.com>
2025-05-13 17:47:38 +0600

    Implemented get request instead of page redirect

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 document.getElementById('downloadBtn').addEventListener('click', (e) => {
     e.preventDefault();
+
     const url = `https://vsix-downloader-mashruf.onrender.com/api/v1/get-vsix?marketplace_url=${document.getElementById('url').value}&version=${document.getElementById('version').value}`;
-    window.location.href = url;
+
+    fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        })
+        .catch(error => console.error('Error downloading file:', error));
 });
