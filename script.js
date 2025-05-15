e29554ab73f579e997202ea6f4d544ddcbad9639
Mashruf <mashrufff@gmail.com>
2025-05-15 23:36:27 +0600

    # Feature - Implemented native browser download functionality

--- script.js
+++ script.js
@@ -3,19 +3,11 @@ document.getElementById('downloadBtn').addEventListener('click', (e) => {
 
     const url = `https://vsix-downloader-mashruf.onrender.com/api/v1/get-vsix?marketplace_url=${document.getElementById('url').value}&version=${document.getElementById('version').value}`;
 
-    fetch(url)
-        .then(response => {
-            const disposition = response.headers.get('Content-Disposition');
-            const filename = disposition.split('filename=')[1];
-            return response.blob().then(blob => ({ blob, filename }));
-        })
-        .then(({ blob, filename }) => {
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = filename;
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        })
-        .catch(error => console.error('Error downloading file:', error));
+    const link = document.createElement('a');
+    link.href = url;
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 });
